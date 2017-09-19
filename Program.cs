0ebd4a7742036390e90779ce0e3842e5cf28abec
Jacob Van Silfhout <jv1@bcs.net.nz>
2017-09-20 10:47:16 +1200

    9960471-problem02-csharp

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _9960471_problem02_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            Console.Write("\n\n");
+            Console.Write("Welcome, this programme will display the division tables of 1 to 12 based on a number input from you");
+            Console.Write("\n\n");
+             Console.WriteLine("Please enter a number below 100 to be calculated:");
+             a = Console.ReadLine();
+             a=Convert.ToInt32(Console.ReadLine());
+        if (num<100);
+            
+            Console.Write("\n\n");
+
+        for (b=1;b=12;b=++);
+            {
+            Console.WriteLine("{0}/{1}={2}\n,n,b,n/a");
+            Console.Readkey();
+            }
+
+        }
+    }
+}
